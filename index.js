a1485160a9dd570397fe68b9999a39c5ddf381c5
x13machine <brandanw2@gmail.com>
2016-05-10 19:43:52 -0500

    syntax errors

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var nouns = require('./nouns.json');
 var adjectives = require('./adjectives.json');
 
-module.exports.generateUsername = function (separator = "") {
-  let noun = nouns[Math.floor(Math.random()*nouns.length)];
-  let adjective = adjectives[Math.floor(Math.random()*adjectives.length)];
-  return noun + separator + adjective;
+module.exports.generateUsername = function (separator) {
+	if(separator==undefined)separator="";
+	var noun = nouns[Math.floor(Math.random()*nouns.length)];
+	var adjective = adjectives[Math.floor(Math.random()*adjectives.length)];
+	return noun + separator + adjective;
 };
